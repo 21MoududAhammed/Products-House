294dc4447a626822bc7a96a955bcb764232d0593
21MoududAhammed <21moudud@gmail.com>
2024-04-03 11:21:45 +0600

    updated readme

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,19 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDEs2e_Q4MNZg8JBJ6Z6rTkhrnEr0cTor4",
-  authDomain: "guestbook-4a04b.firebaseapp.com",
-  projectId: "guestbook-4a04b",
-  storageBucket: "guestbook-4a04b.appspot.com",
-  messagingSenderId: "952980051558",
-  appId: "1:952980051558:web:1d580858079669bc6abfa7",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-
-
 
 export { auth };
