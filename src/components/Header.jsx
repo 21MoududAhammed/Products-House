e6139c4dd321994f346665ec6723938b11bb5ebc
21MoududAhammed <21moudud@gmail.com>
2024-04-03 23:11:44 +0600

    design correction with log out button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -159,8 +159,8 @@ export default function Header() {
         {/* profile end  */}
 
         {isLoggedIn ? (
-          <NavLink className=" me-1 md:me-2" onClick={handleLogOut}>
-            <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+          <NavLink className="  md:me-2" onClick={handleLogOut}>
+            <button className=" px-2 md:px-6 py-2 text-sm md:font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 Log Out
               </button>
           </NavLink>
