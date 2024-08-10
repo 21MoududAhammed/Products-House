bfcd94f2ee3453c8409129419bbe6aac18e16c3b
21MoududAhammed <21moudud@gmail.com>
2024-08-10 10:55:47 +0600

    Reform the routing of this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import Layout from "./components/Layout";
-// import Home from "./components/Home";
-// import Contact from "./components/Contact";
-// import About from "./components/About";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home/Home";
+import AllProducts from "./components/AllProducts";
+import Cart from "./components/Cart";
+import LogIn from "./components/LogIn";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import ProductDetails from "./components/ProductDetails";
+import CategoryDisplay from "./components/categories/CategoryDisplay";
+import NotFound from "./components/NotFound";
+import Profile from "./components/Profile";
 
-// export default function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/contact" element={<Contact />} />
-//           </Route>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<AllProducts />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/products/product-details/:p_id"
+          element={<ProductDetails />}
+        />
+        <Route path="/:category" element={<CategoryDisplay />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Layout from "./components/Layout.jsx";
-import Cart from "./components/Cart.jsx";
-import LogIn from "./components/LogIn.jsx";
-import Register from "./components/Register.jsx";
-import Reset from "./components/Reset.jsx";
 import AuthProvider from "./providers/AuthProvider.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Profile from "./components/Profile.jsx";
 import CartProvider from "./providers/CartProvider.jsx";
-import ProductDetails from "./components/ProductDetails.jsx";
-import { loadProduct } from "./loaders/index.js";
-import AllProducts from "./components/AllProducts.jsx";
-import CategoryDisplay from "./components/categories/CategoryDisplay";
-import NotFound from "./components/NotFound";
-import Home from "./components/Home/Home";
-
-
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<AllProducts />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/reset" element={<Reset />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route
-        path="/products/product-details/:p_id"
-        element={<ProductDetails/>}
-      />
-      <Route path="/:category" element={<CategoryDisplay />} />
-      <Route path="*" element={<NotFound />}></Route>
-    </Route>
-  )
-);
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <CartProvider>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </CartProvider>
       <ToastContainer />
     </AuthProvider>
