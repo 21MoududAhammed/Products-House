2df02ea1002115bd3ce4230397f832f6219e40d1
21MoududAhammed <21moudud@gmail.com>
2024-08-10 10:58:33 +0600

    Added md:mx-5 in layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div >
+    <div className="md:mx-5">
       <Header />
       <div className="max-w-screen-2xl mx-auto">
         <Outlet />
